08aaff4cc5aaac684f61397fe3482f42e5ffb035
alexeusgr <alexeusgr@gmail.com>
2022-08-19 10:23:14 +0300

    add ignore, main flake

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
       # local directories (for absolute paths you can omit 'path:')
       a-flake.url = "path:./a-flake";
       b-flake.url = "path:./b-flake";
+    };
   outputs = { self, nixpkgs }: {
 
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
