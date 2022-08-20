8a155daecffdc278217f3750f53fbaa3d5ac0075
alexeusgr <alexeusgr@gmail.com>
2022-08-20 14:33:43 +0300

    simplified

--- flake.nix
+++ flake.nix
@@ -1,18 +1,19 @@
+
 {
   description = "A very basic flake";
   inputs = {
-      nixpkgs.url = "github:NixOS/nixpkgs";
+      #nixpkgs.url = "github:NixOS/nixpkgs";
       # flake-utils.url = "github:numtide/flake-utils";
       # local directories (for absolute paths you can omit 'path:')
-      # a-flake.url = "path:./a-flake";
-      # b-flake.url = "path:./b-flake";
+      a-flake.url = "path:./a-flake";
+      b-flake.url = "path:./b-flake";
     };
-  outputs = { self, nixpkgs, }: 
+  outputs = { self, }: 
     {
-    devShell = nixpkgs.mkShell {
-          buildInputs = with nixpkgs; [
-          ];
-          #inputsFrom = builtins.attrValues self.packages.${system};
-        }; 
+#    devShell = nixpkgs.mkShell {
+#          buildInputs = with nixpkgs; [
+#          ];
+          inputsFrom = builtins.attrValues self.packages.${system};
+#        }; 
     };
 }
