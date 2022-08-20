22c181549bf32c3b8cc5347c4ab77127ccae4659
alexeusgr <alexeusgr@gmail.com>
2022-08-20 10:32:34 +0300

    empty flake

--- flake.nix
+++ flake.nix
@@ -1,16 +1,18 @@
 {
   description = "A very basic flake";
   inputs = {
+      nixpkgs.url = "github:NixOS/nixpkgs";
+      # flake-utils.url = "github:numtide/flake-utils";
       # local directories (for absolute paths you can omit 'path:')
-      a-flake.url = "path:./a-flake";
-      b-flake.url = "path:./b-flake";
+      # a-flake.url = "path:./a-flake";
+      # b-flake.url = "path:./b-flake";
     };
-  outputs = { self, nixpkgs, a-flake, b-flake }: {
-  
-    stdenv.mkDerivation {
-      name = "libfoo-1.2.3";
-      ...
-      buildInputs = [a-flake b-flake];
-  }
-  };
+  outputs = { self, nixpkgs, a-flake, b-flake }: 
+    {
+    devShell = nixpkgs.mkShell {
+          buildInputs = with nixpkgs; [
+          ];
+          #inputsFrom = builtins.attrValues self.packages.${system};
+        }; 
+    };
 }
