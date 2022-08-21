bb6e37e9cc26d7db02b0e798d51bec286f2e3653
alexeusgr <alexeusgr@gmail.com>
2022-08-21 16:09:35 +0300

    simple flake -> two small

--- b-flake/flake.nix
+++ b-flake/flake.nix
@@ -1,34 +1,11 @@
 {
-  outputs = { self }: {
-    zap = "zip";
-  };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs"; };
+
+  outputs = { self, nixpkgs }:
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+      packages.x86_64-linux.hello = pkgs.hello;
+      devShell.x86_64-linux =
+        pkgs.mkShell { buildInputs = [ self.packages.x86_64-linux.hello pkgs.cowsay ]; };
+   };
 }
-#{
-#    inputs = {
-#        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-#        utils.url = "github:numtide/flake-utils";
-#    };
-#
-#    outputs = {self, nixpkgs, utils}:
-#    let out = system:
-#    let pkgs = nixpkgs.legacyPackages."${system}";
-#    in {
-#
-#        devShell = pkgs.mkShell {
-#            buildInputs = with pkgs; [
-#                python3Packages.poetry
-#            ];
-#        };
-#
-#        defaultPackage = with pkgs.poetry2nix; mkPoetryApplication {
-#            projectDir = ./.;
-#            preferWheels = true;
-#        };
-#
-#        defaultApp = utils.lib.mkApp {
-#            drv = self.defaultPackage."${system}";
-#        };
-#
-#    }; in with utils.lib; eachSystem defaultSystems out;
-#
-#}
--- flake.nix
+++ flake.nix
@@ -1,15 +1,17 @@
-
 {
   description = "A very basic flake";
   inputs = {
-      #nixpkgs.url = "github:NixOS/nixpkgs";
       # flake-utils.url = "github:numtide/flake-utils";
       # local directories (for absolute paths you can omit 'path:')
       a-flake.url = "path:./a-flake";
       b-flake.url = "path:./b-flake";
     };
-  outputs = { self, }: 
+  outputs = { self, nixpkgs, }: 
     {
-
+    devShell = nixpkgs.mkShell {
+          buildInputs = with nixpkgs; [
+          ];
+          #inputsFrom = builtins.attrValues self.packages.${system};
+        }; 
     };
 }
