1b3074dd39fa0816fe9087d094c43b201bb3e4c6
alexeusgr <alexeusgr@gmail.com>
2022-08-22 09:08:24 +0300

    separated, how to combine?

--- flake.nix
+++ flake.nix
@@ -1,17 +1,18 @@
 {
   description = "A very basic flake";
   inputs = {
+      #nixpkgs.url = "github:nixos/nixpkgs"; 
       # flake-utils.url = "github:numtide/flake-utils";
       # local directories (for absolute paths you can omit 'path:')
       a-flake.url = "path:./a-flake";
       b-flake.url = "path:./b-flake";
     };
-  outputs = { self, nixpkgs, }: 
+  outputs = { self, nixpkgs }: 
     {
     devShell = nixpkgs.mkShell {
           buildInputs = with nixpkgs; [
           ];
-          #inputsFrom = builtins.attrValues self.packages.${system};
-        }; 
+          inputsFrom = [self.devShell.a-flake self.devShell.b-flake];
     };
+  };
 }
