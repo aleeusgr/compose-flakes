effac9bd625ecfce73cc19e4a40e9d13b7b2069d
alexeusgr <alexeusgr@gmail.com>
2022-08-22 08:33:40 +0300

    simplify

--- a-flake/flake.nix
+++ a-flake/flake.nix
@@ -6,6 +6,6 @@
     in {
       packages.x86_64-linux.hello = pkgs.hello;
       devShell.x86_64-linux =
-        pkgs.mkShell { buildInputs = [ self.packages.x86_64-linux.hello pkgs.cowsay ]; };
+        pkgs.mkShell { buildInputs = [ pkgs.cowsay ]; };
    };
 }
