46db6b7c30da204850367e2f06858059c7b64deb
alexeusgr <alexeusgr@gmail.com>
2022-08-23 09:30:34 +0300

    added flakes to argset

--- flake.nix
+++ flake.nix
@@ -1,13 +1,13 @@
 {
   description = "A very basic flake";
   inputs = {
-      #nixpkgs.url = "github:nixos/nixpkgs"; 
+      nixpkgs.url = "github:nixos/nixpkgs"; 
       # flake-utils.url = "github:numtide/flake-utils";
       # local directories (for absolute paths you can omit 'path:')
       a-flake.url = "path:./a-flake";
       b-flake.url = "path:./b-flake";
     };
-  outputs = { self, nixpkgs }: 
+  outputs = { self, nixpkgs, a-flake, b-flake }: 
     {
     devShell = nixpkgs.mkShell {
           buildInputs = with nixpkgs; [
