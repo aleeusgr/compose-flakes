0e401bd495d1f6bd7ebc807d4cd2712921e5bd16
alexeusgr <alexeusgr@gmail.com>
2022-08-29 09:17:21 +0300

    useless main, but runs

--- flake.nix
+++ flake.nix
@@ -9,13 +9,14 @@
     };
   outputs = { self, nixpkgs, a-flake, b-flake } @ inputs: 
   {
-
+    # let nixpkgs = nixpkgs.legacyPackages.x86_64-linux
+    # in  
     devShell.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.mkShell 
     {
-    #      buildInputs = with nixpkgs; [];
-    #       inputsFrom = [self.devShell.a-flake self.devShell.b-flake]; #error: attribute 'a-flake' missing
-    #       inherit (inputs.a-flake) devShell;                          #error: cannot coerce a set to a string
-    #       inherit (inputs.a-flake) mkShell;                           #error: attribute mkShell is missing
+    buildInputs = with nixpkgs; [];    
+    # inputsFrom = [self.devShell.a-flake self.devShell.b-flake]; #error: attribute 'a-flake' missing
+    # inherit (inputs.a-flake) devShell;                          #error: cannot coerce a set to a string
+    # inherit (inputs.a-flake) mkShell;                           #error: attribute mkShell is missing
     };
   };
 }
